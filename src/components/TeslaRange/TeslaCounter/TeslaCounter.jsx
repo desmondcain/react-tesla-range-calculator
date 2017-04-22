3e58abd834e6936a692415766b7973b59df00d15
Desmond Cain <desmondcain@gmail.com>
2017-04-22 18:41:29 -0500

    Add TeslaCounter styles and markup

--- src/components/TeslaRange/TeslaCounter/TeslaCounter.jsx
+++ src/components/TeslaRange/TeslaCounter/TeslaCounter.jsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+import './TeslaCounter.css'
+
 class TeslaConter extends Component {
 
   render() {
+    let title = this.props.title
+    let value = this.props.value
+    let unit  = this.props.unit
+
     return (
-      <div></div>
+      <div className="tesla-counter">
+        <p className="tesla-counter-title">{title}</p>
+        <div className="tesla-counter-container cf">
+          <div className="tesla-counter-item" tabIndex="0">
+            <p className="tesla-counter-number">
+              {value}
+              <span>{unit}</span>
+            </p>
+            <div className="tesla-counter-controls" tabIndex="-1">
+              <button tabIndex="-1"></button>
+              <button tabIndex="-1"></button>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/components/TeslaRange/index.jsx
+++ src/components/TeslaRange/index.jsx
@@ -3,6 +3,9 @@ import rangeModel from '../../data/rangeModel';
 import TeslaRangeHeader from './TeslaRangeHeader/TeslaRangeHeader';
 import TeslaRangeCar from './TeslaRangeCar/TeslaRangeCar';
 import TeslaStats from './TeslaStats/TeslaStats';
+import TeslaCounter from './TeslaCounter/TeslaCounter';
+
+import './index.css';
 
 const TESLA_MODELS = ['60', '60D', '75', '75D', '90D', 'P100D']
 
@@ -31,14 +34,20 @@ class TeslaRange extends Component {
     const stats = this.stats;
 
     return (
-      <div>
+      <div className="tesla-range">
         <TeslaRangeHeader />
         <TeslaRangeCar wheelSize={defaultSettings.wheels} />
         <TeslaStats stats={stats} />
 
-        <div className="disclaimer-range">
-          <p>The actual amount of range that you experience will vary based on your particular use conditions. See how particular use conditions may affect your range in our simulation model.</p>
-          <p>Vehicle range may vary depending on the vehicle configuration, battery age and condition, driving style and operating, environmental and climate conditions.</p>
+        <div className="tesla-controlls">
+          <TeslaCounter title="Speed" unit="mph" step={5} min={45} max={70} />
+        </div>
+
+        <div className="row">
+          <p className="notice-range">
+            The actual amount of range that you experience will vary based on your particular use conditions. See how particular use conditions may affect your range in our simulation model.
+            Vehicle range may vary depending on the vehicle configuration, battery age and condition, driving style and operating, environmental and climate conditions.
+          </p>
         </div>
       </div>
     );
