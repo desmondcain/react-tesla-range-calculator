685dd3b0a578aed51db0060b07a326014784b219
Desmond Cain <desmondcain@gmail.com>
2017-04-22 16:41:31 -0500

    Calculate stats and populat stats component list

--- src/components/TeslaRange/TeslaStats/TeslaStats.jsx
+++ src/components/TeslaRange/TeslaStats/TeslaStats.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import { array } from 'prop-types';
 
 class TeslaStats extends Component {
 
   render() {
+    let stats = this.props.stats;
+
     return (
-      <div className="tesla-stats"></div>
+      <div className="tesla-stats">
+        <ul>
+          {stats.map(stat =>
+            <li key={stat.model}>
+              <div className={`tesla-stats-icon tesla-stats-icon--${stat.model.toLowerCase()}`}></div>
+              <p>{stat.miles}</p>
+            </li>
+          )}
+        </ul>
+      </div>
     );
   }
+}
+
+TeslaStats.propTypes = {
+  stats: array.isRequired
 }
 
 export default TeslaStats;
--- src/components/TeslaRange/index.jsx
+++ src/components/TeslaRange/index.jsx
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import { string, number } from 'prop-types';
+import rangeModel from '../../data/rangeModel';
 import TeslaRangeHeader from './TeslaRangeHeader/TeslaRangeHeader';
 import TeslaRangeCar from './TeslaRangeCar/TeslaRangeCar';
 import TeslaStats from './TeslaStats/TeslaStats';
 
-const TESLA_MODELS = ['60', '60D', '75', '75D', '90D', 'P100D'];
+const TESLA_MODELS = ['60', '60D', '75', '75D', '90D', 'P100D']
+
+function calculateStats(settings, models) {
+  return models.map(model => {
+    let {climate, speed, temperature, wheels} = settings
+    let miles = rangeModel[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature]
+
+    return {
+      model,
+      miles
+    };
+  })
+}
 
 class TeslaRange extends Component {
+  constructor() {
+    super()
+
+    this.defaultSettings = { climate: true, speed: 55, temperature: 20, wheels: 19 }
+    this.stats = calculateStats(this.defaultSettings, TESLA_MODELS)
+  }
 
   render() {
+    let stats = this.stats
+
     return (
       <div>
         <TeslaRangeHeader />
         <TeslaRangeCar />
-        <TeslaStats />
+        <TeslaStats stats={stats} />
 
         <div className="disclaimer-range">
           <p>The actual amount of range that you experience will vary based on your particular use conditions. See how particular use conditions may affect your range in our simulation model.</p>
